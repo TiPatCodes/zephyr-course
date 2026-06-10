ce0a19b6d099a7dade72e207a278a7331e8d83e4
Tirth Patel <tirthpatel199561@gmail.com>
2026-06-09 22:58:22 -0400

    * Assignment Lec - 6

---
+++ app/drivers/our_driver/our_driver.c
@@ -0,0 +1,35 @@
+#include "zephyr/device.h"
+#include "zephyr/logging/log_core.h"
+#include <zephyr/logging/log.h>
+#include <zephyr/drivers/sensor.h>
+
+#define DT_DRV_COMPAT our_driver
+
+LOG_MODULE_REGISTER(our_driver, LOG_LEVEL_INF);
+
+// custom api wrapped on the standard available api of sensor.h  
+static  int  channel_get_my_imp(const struct device *dev,
+				    enum sensor_channel chan,
+				    struct sensor_value *val)
+{
+
+    LOG_INF("Hello from Channel Get, channel %d", chan);
+    return 0;
+}
+
+
+static DEVICE_API(sensor, api_iomico_lecture) = {
+    .channel_get = channel_get_my_imp,
+};
+
+
+// Init fn
+static int init( const struct device* dev){
+    LOG_INF("Device Initialization");
+    return 0;
+}
+
+
+
+DEVICE_DT_INST_DEFINE(0, init, NULL , NULL, NULL, POST_KERNEL_1, 80 , &api_iomico_lecture)
+
