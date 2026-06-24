e45d35d9552e53163a286b8d0834993a30de1c6f
Tirth Patel <tirthpatel199561@gmail.com>
2026-06-23 20:06:25 -0400

    *l8-task1 complete

---
+++ twister-out/qemu_x86_atom/zephyr_gnu/zephyr-course/tests/ring_buf/homework.unit.ring_buf/Kconfig/kconfig_module_dirs.cmake
@@ -0,0 +1,3 @@
+set(kconfig_env_dirs)
+list(APPEND kconfig_env_dirs ZEPHYR_CMSIS_6_MODULE_DIR=C:/zephyrtwister/deps/modules/hal/cmsis_6)
+list(APPEND kconfig_env_dirs ZEPHYR_HAL_STM32_MODULE_DIR=C:/zephyrtwister/deps/modules/hal/stm32)
---
+++ twister-out/qemu_x86_atom/zephyr_gnu/zephyr-course/tests/ring_buf/homework.unit.ring_buf/zephyr/zephyr.dts.d
@@ -0,0 +1,7 @@
+empty_file.o: C:/zephyrtwister/deps/zephyr/misc/empty_file.c \
+ C:/zephyrtwister/deps/zephyr/boards/qemu/x86/qemu_x86.dts \
+ C:/zephyrtwister/deps/zephyr/dts/common/mem.h \
+ C:/zephyrtwister/deps/zephyr/dts/x86/intel/atom.dtsi \
+ C:/zephyrtwister/deps/zephyr/dts/common/skeleton.dtsi \
+ C:/zephyrtwister/deps/zephyr/include/zephyr/dt-bindings/interrupt-controller/intel-ioapic.h \
+ C:/zephyrtwister/deps/zephyr/include/zephyr/dt-bindings/pcie/pcie.h
